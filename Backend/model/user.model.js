619865735a047a80929695b2990973ac19b5df96
Parineeta Pareek <parineetapareek215@gmail.com>
2024-04-11 21:59:16 +0530

    changes in schema of user

--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,21 +1,24 @@
-import mongoose  from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-    fullname:{
-        type:String,
-        required:true
+const userSchema = mongoose.Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    
-})
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+
 export default User;
